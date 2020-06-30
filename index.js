ac91c4b74b3eca861b2530978c52a4b57ad9911b
Alexey Ponomaryov <alexatiks@gmail.com>
2020-06-30 16:05:25 +0300

    Add eslint

--- index.js
+++ index.js
@@ -5,80 +5,76 @@ const Quote = require('./model/Quote');
  * Used to interact with the Yahoo Finance API.
  */
 class Yahoo {
-
-    _get(url) {
-        return new Promise((resolve, reject) => {
-            const req = https.get(url, res => {
-                let body = '';
-                res.on('data', data => {
-                    body += data;
-                });
-                res.on('end', () => {
-                    resolve(JSON.parse(body));
-                });
-            });
-            req.on('error', e => {
-                reject(e);
-            });
+  _get(url) {
+    return new Promise((resolve, reject) => {
+      const req = https.get(url, (res) => {
+        let body = '';
+        res.on('data', (data) => {
+          body += data;
         });
-    }
+        res.on('end', () => {
+          resolve(JSON.parse(body));
+        });
+      });
+      req.on('error', (e) => {
+        reject(e);
+      });
+    });
+  }
 
-    /**
+  /**
      * Returns an array of Quote model from Yahoo Finance.
      * @constructor
      * @param {String} symbol
-     * @param args interval - How long each quote should represent: [1m, 2m, 5m, 15m, 30m, 60m, 90m, 1h, 1d, 5d, 1wk, 1mo, 3mo]
+     * @param args interval
      * @returns {Promise<Array>}
      */
-    getHistory(symbol, args) {
-        return new Promise((resolve, reject) => {
-            let options = {
-                interval: '1d',
-                period1: 0,
-                period2: 9999999999,
-                ...args,
-            };
+  getHistory(symbol, args) {
+    return new Promise((resolve, reject) => {
+      const options = {
+        interval: '1d',
+        period1: 0,
+        period2: 9999999999,
+        ...args,
+      };
 
-            this._get(
-                `https://query2.finance.yahoo.com/v8/finance/chart/${symbol}?period1=${
-                    options.period1
-                }&period2=${
-                    options.period2
-                }&interval=${
-                    options.interval
-                }&indicators=quote&includeTimestamps=true&includePrePost=true&events=div%7Csplit%7Cearn`
-            ).then(body => {
+      this._get(
+        `https://query2.finance.yahoo.com/v8/finance/chart/${symbol}?period1=${
+          options.period1
+        }&period2=${
+          options.period2
+        }&interval=${
+          options.interval
+        }&indicators=quote&includeTimestamps=true&includePrePost=true&events=div%7Csplit%7Cearn`
+      ).then((body) => {
+        const json = body.chart.result[0];
+        const array = [];
 
-                let json = body.chart.result[0];
-                let array = [];
+        const timestamps = json.timestamp;
+        const quotes = json.indicators.quote[0];
 
-                const timestamps = json.timestamp;
-                const quotes = json.indicators.quote[0];
+        if (timestamps === undefined) reject(new Error('Requested range is invalid'));
 
-                if (timestamps === undefined) reject(new LibraryError("Invalid range given. Yahoo suggests using: " + json.meta.validRanges + " (Is this stock too young?)"));
-                else {
-                    Object.keys(timestamps).forEach(key => {
-                        if (quotes[key] !== null) array.push(
-                            new Quote({
-                                symbol: symbol,
-                                date: new Date(timestamps[key] * 1000),
-                                price: {
-                                    open: Number(quotes.open[key]),
-                                    high: Number(quotes.high[key]),
-                                    low: Number(quotes.low[key]),
-                                    close: Number(quotes.close[key]),
-                                    volume: Number(quotes.volume[key])
-                                }
-                            })
-                        )
-                    });
-                    resolve(array);
-                }
-            }).catch(reject);
+        Object.keys(timestamps).forEach((key) => {
+          if (quotes[key] !== null) array.push(
+            new Quote({
+              symbol: symbol,
+              date: new Date(timestamps[key] * 1000),
+              price: {
+                open: Number(quotes.open[key]),
+                high: Number(quotes.high[key]),
+                low: Number(quotes.low[key]),
+                close: Number(quotes.close[key]),
+                volume: Number(quotes.volume[key])
+              }
+            })
+          );
         });
-    }
+
+        resolve(array);
+      }).catch(reject);
+    });
+  }
 }
 
 module.exports = Yahoo;
-
-
--- model/Quote.js
+++ model/Quote.js
@@ -1,52 +1,52 @@
 class Quote {
-	/**
-	 * Creates a new Quote instance.
-	 * @constructor
-	 * @param {Object} object
-	 * @property {String} symbol
-	 * @property {Date} date
-	 * @property {String} source
-	 * @property {Object} price
-	 * @property {Number} price.open
-	 * @property {Number} price.high
-	 * @property {Number} price.low
-	 * @property {Number} price.close
-	 * @property {Number} price.volume
-	 */
-	constructor(object) {
-		this.symbol = object.symbol;
-		this.date = object.date;
-		this.source = object.source;
-		this.price = object.price;
-	}
+  /**
+   * Creates a new Quote instance.
+   * @constructor
+   * @param {Object} object
+   * @property {String} symbol
+   * @property {Date} date
+   * @property {String} source
+   * @property {Object} price
+   * @property {Number} price.open
+   * @property {Number} price.high
+   * @property {Number} price.low
+   * @property {Number} price.close
+   * @property {Number} price.volume
+   * */
+  constructor(object) {
+    this.symbol = object.symbol;
+    this.date = object.date;
+    this.source = object.source;
+    this.price = object.price;
+  }
 
-	getSymbol() {
-		return this.symbol;
-	}
+  getSymbol() {
+    return this.symbol;
+  }
 
-	getDate() {
-		return this.date;
-	}
+  getDate() {
+    return this.date;
+  }
 
-	getOpen() {
-		return this.price.open;
-	}
+  getOpen() {
+    return this.price.open;
+  }
 
-	getHigh() {
-		return this.price.high;
-	}
+  getHigh() {
+    return this.price.high;
+  }
 
-	getLow() {
-		return this.price.low;
-	}
+  getLow() {
+    return this.price.low;
+  }
 
-	getClose() {
-		return this.price.close;
-	}
+  getClose() {
+    return this.price.close;
+  }
 
-	getVolume() {
-		return this.price.volume;
-	}
+  getVolume() {
+    return this.price.volume;
+  }
 }
 
 module.exports = Quote;
